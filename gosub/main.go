546d6b65c7275d540f7e5c293a8031a814a74f6a
blumid <shhapip13@gmail.com>
2023-03-05 17:46:51 +0330

    cleaning code

--- gosub/main.go
+++ gosub/main.go
@@ -34,36 +34,6 @@ func worker(domain string, commands map[int]string, wg *sync.WaitGroup, signal c
 		time.Sleep(time.Millisecond * 150)
 		tracker.Increment(1)
 	}
-
-	// Round 1:
-
-	// item.runCommand("assetfinder -subs-only "+domain+" | anew > "+outdir+"/assetfinder", "assetfinder", signal)
-	// item.runCommand("subfinder -d "+domain+" -o "+outdir+"/subfinder", "subfinder", signal)
-	// item.runCommand("amass enum -passive -d "+domain+" > "+outdir+"/amass", "amass", signal)
-
-	// item.runCommand("cat "+outdir+"/assetfinder "+outdir+"/subfinder "+outdir+"/amass | deduplicate --sort > "+outdir+"/round1", "", signal)
-	// item.runCommand("rm -f "+outdir+"/assetfinder "+outdir+"/subfinder "+outdir+"/amass 2>/dev/null", "", signal)
-
-	// time.Sleep(time.Second * 2)
-
-	// Round 2
-
-	// runCommand("cp " + wordlist + " " + outdir + "/shuffle")
-
-	// runCommand("sed -e \"s/$/.${" + domain + "##*\\/}/\"  -i " + outdir + "/shuffle")
-
-	// runCommand("dnsx -list " + outdir + "/shuffle -r " + resolver + "-silent -o " + outdir + "/step1")
-
-	// runCommand("cat " + outdir + "/step1 | anew -q " + outdir + "/round1")
-
-	// runCommand("gotator -silent -sub " + outdir + "/round1 -depth 2 -mindup > " + outdir + "/gotator")
-
-	// runCommand("dnsx -list " + outdir + "/gotator -r " + resolver + " -silent -o " + outdir + "/step2")
-
-	// runCommand("cat " + outdir + "/step1 " + outdir + "/step2 | deduplicate --sort > " + outdir + "/final")
-
-	// results <- item
-
 	time.Sleep(time.Second * 2)
 	wg.Done()
 }
@@ -81,13 +51,20 @@ type result struct {
 	domain string
 }
 
-func initialCommands(outdir string) map[int]string {
+func initialCommands(outdir string, wordlist string) map[int]string {
 	commands := map[int]string{
-		0: "assetfinder -subs-only  %[1]s | anew > " + outdir + "/%[1]s" + "/assetfinder",
-		1: "subfinder -d %[1]s -o " + outdir + "/%[1]s" + "/subfinder",
-		2: "amass enum -passive -d %[1]s > " + outdir + "/%[1]s" + "/amass",
-		3: "cat " + outdir + "/%[1]s" + "/assetfinder " + outdir + "/%[1]s" + "/subfinder " + outdir + "/%[1]s" + "/amass | deduplicate --sort > " + outdir + "/%[1]s" + "/round1",
-		4: "rm -f " + outdir + "/%[1]s" + "/assetfinder " + outdir + "/%[1]s" + "/subfinder " + outdir + "/%[1]s" + "/amass 2>/dev/null",
+		0:  "assetfinder -subs-only  %[1]s | anew > " + outdir + "/%[1]s" + "/assetfinder",
+		1:  "subfinder -d %[1]s -o " + outdir + "/%[1]s" + "/subfinder",
+		2:  "amass enum -passive -d %[1]s > " + outdir + "/%[1]s" + "/amass",
+		3:  "cat " + outdir + "/%[1]s" + "/assetfinder " + outdir + "/%[1]s" + "/subfinder " + outdir + "/%[1]s" + "/amass | deduplicate --sort > " + outdir + "/%[1]s" + "/round1",
+		4:  "rm -f " + outdir + "/%[1]s" + "/assetfinder " + outdir + "/%[1]s" + "/subfinder " + outdir + "/%[1]s" + "/amass 2>/dev/null",
+		5:  "cp " + wordlist + " " + outdir + "/%[1]s" + "/shuffle",
+		6:  "sed -e \"s/$/.${%[1]s##*\\/}/\"  -i " + outdir + "/%[1]s" + "/shuffle",
+		7:  "dnsx -list " + outdir + "/%[1]s" + "/shuffle -silent -o " + outdir + "/%[1]s" + "/step1",
+		8:  "cat " + outdir + "/%[1]s" + "/step1 | anew -q " + outdir + "/%[1]s" + "/round1",
+		9:  "gotator -silent -sub " + outdir + "/%[1]s" + "/round1 -depth 2 -mindup > " + outdir + "/%[1]s" + "/gotator",
+		10: "dnsx -list " + outdir + "/%[1]s" + "/gotator -r " + resolver + " -silent -o " + outdir + "/%[1]s" + "/step2",
+		11: "cat " + outdir + "/%[1]s" + "/step1 " + outdir + "/%[1]s" + "/step2 | deduplicate --sort > " + outdir + "/%[1]s" + "/final",
 	}
 
 	return commands
@@ -115,7 +92,7 @@ func main() {
 
 	signal := make(chan int, 1)
 
-	commands := initialCommands(output)
+	commands := initialCommands(output, wordlist)
 
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
