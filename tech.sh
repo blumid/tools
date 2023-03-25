0b3b00180c4ca5357a5f0b64368995c9408dba72
blumid <shhapip13@gmail.com>
2023-03-25 17:38:15 +0330

    adding tech.sh

--- tech.sh
+++ tech.sh
@@ -2,14 +2,50 @@
 
 #Usage : cat rare_subs | tech.sh
 
-cat /dev/stdin | httpx -silent -output httpx > origin
+# cat /dev/stdin | httpx -silent -output httpx > origin
 
-webanalyze -update -silent
+# get techknowledgyfile.json
+webanalyze -update
 
 webanalyze -hosts httpx -output json -silent | jq -c '{ hostname:.hostname , lang: [.matches[] | select(.app.category_names | contains(["Programming languages"]))["app_name"]]  , server: [.matches[] | select(.app.category_names | contains(["Web servers"]))["app_name"]] , cms: [.matches[] | select(.app.category_names | contains(["CMS"]))["app_name"]] }' >result
 
 # maybe useful:
 # cat result | jq '. | if .lang[0] != null then .hostname  else null end'
+
+
+
+run_ffuf(){
+
+    case $2 in
+        php)
+            wordlist=~/.cache/kiterunner/wordlists/php.txt
+            ;;
+        asp)
+            wordlist=~/.cache/kiterunner/wordlists/httparchive_aspx_asp_cfm_svc_ashx_asmx_2022_08_28.txt
+            ;;
+        jsp)
+            wordlist=~/.cache/kiterunner/wordlists/jsp.txt
+            ;;
+        all)
+            wordlist=~/.cache/kiterunner/wordlists/raft-large-files.txt
+            ;;
+        *)
+            echo "fuck!"
+            exit(0)
+            ;;
+    esac
+    wordlist=$2
+    ffuf -w $wordlist -X GET -u $1/FUZZ -mc 200,405,500 -r -t 5 -ac -v -c -o "$1"_ffuf.json
+
+    echo $1 >> f1
+}
+
+detect(){
+    ## run ffuf on $1
+    # ffuf -w $wordlist -X GET -u $1/FUZZ -mc 200,405,500 -r -t 5 -ac -v -c -o "$1"_ffuf.json
+
+
+}
 
 filter() {
 
@@ -18,40 +54,45 @@ filter() {
     #     "PHP")
     # esac
 
-############## first filter #############
+    ############## first filter #############
 
     ### php ###
     cat "result" | jq -r '. | select(.lang[0] == "PHP") | .hostname' | while read line; do
         echo -e "for php : \n $line"
-        echo $line >> f1
+        run_ffuf $line php
     done
 
     ### asp ###
-    cat "result" | jq -r '. | select(.lang[0] == "ASP") | .hostname' | while read line; do
+    cat "result" | jq -r '. | select((.lang[0] == "ASP") or (.lang[0] == "ASPX")) | .hostname' | while read line; do
         echo -e "for asp : \n $line"
-        echo $line >> f1
+        run_ffuf $line asp
     done
 
     ### jsp ###
     cat "result" | jq -r '. | select(.lang[0] == "JSP") | .hostname' | while read line; do
         echo -e "for jsp : \n $line"
-        echo $line >> f1
+        run_ffuf $line jsp
     done
 
-    ### python ###
-    cat "result" | jq -r '. | select(.lang[0] == "PYTHON") | .hostname' | while read line; do
-        echo -e "for python : \n $line"
-        echo $line >> f1
-    done
+    # cat "result" | jq -r '. | select(.lang[0] == "PYTHON") | .hostname' | while read line; do
+    #     echo -e "for python : \n $line"
+    #     run_ffuf $line python
+    # done
+
+    ### node.js ###
 
 
 
-############## second filter #############
+
+    ############## second filter #############
     cat "origin" | anew -d "f1" | while read line; do
-
-
+    # run ffuf with mixed wordlist on these
+        # detect $line
+        # echo $line
+        run_ffuf $line all
     done
 
 }
+
 
 filter
