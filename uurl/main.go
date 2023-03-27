e6b2a9ebf03db6014736d0842a783bfdf97b759e
blumid <shhapip13@gmail.com>
2023-03-28 02:10:37 +0330

    new idea, phase one

--- uurl/main.go
+++ uurl/main.go
@@ -7,12 +7,10 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
-	"reflect"
 	"strings"
 )
 
 func openFile() (*os.File, error) {
-	// fmt.Println("opening file:")
 
 	pwd, err := filepath.Abs(".")
 	if err != nil {
@@ -28,74 +26,45 @@ func openFile() (*os.File, error) {
 	return file, nil
 
 }
-func getKeysPath(URL string) ([]string, string) {
+
+func getKeysPath(URL string) string {
 	u, _ := url.Parse(URL)
 	path := u.Scheme + "://" + u.Host + u.Path
-
-	// fmt.Println("path is:", path)
 
 	keys := make([]string, 0, len(u.Query()))
 	for k := range u.Query() {
 		keys = append(keys, k)
 	}
-	// fmt.Println("keys are:", keys)
 
-	return keys, path
-
-}
-
-func addTo(file *os.File, URL string) bool {
-
-	keys, path := getKeysPath(URL)
-
-	sc := bufio.NewScanner(file)
-	sc.Split(bufio.ScanLines)
-
-	fmt.Println("sc scan is:", sc.Scan())
-	for sc.Scan() {
-		fmt.Println("in side of addTo, for scan")
-		patt_keys, patt_path := getKeysPath(sc.Text())
-		if path == patt_path {
-			fmt.Println("first condition: ", path)
-			if reflect.DeepEqual(keys, patt_keys) {
-				fmt.Println("duplicate happend.", URL)
-				return false
-			}
-		} else {
-			continue
-		}
+	if len(keys) != 0 {
+		return path + "?" + strings.Join(keys, "&")
+	} else {
+		return path
 	}
-	return true
-}
 
-type pattern struct {
-	hname string
-	query []string
 }
 
 func main() {
+	// file, err := openFile()
 
-	file, err := openFile()
-	// dw := bufio.NewWriter(file)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
 
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	lines := make(map[string]bool)
 
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
 		url := strings.TrimSpace(sc.Text())
 
-		if addTo(file, url) {
-			// dw.WriteString(url + "\n")
-			if _, err := file.WriteString(url + "\n"); err != nil {
-				fmt.Println("err is: ", err)
-			}
+		res := getKeysPath(url)
+
+		if !lines[res] {
+			lines[res] = true
+			fmt.Println(url)
 		}
 
 	}
-	// defer dw.Flush()
-	defer file.Close()
 
 }
