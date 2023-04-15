53766bb18ef030ee25a5dccb4b4525bc7f7f7ca7
blumid <shhapip13@gmail.com>
2023-04-15 16:26:09 +0330

    start phase  adding parse XHR in js file

--- gfp/main.go
+++ gfp/main.go
@@ -4,10 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/geziyor/geziyor"
 	"github.com/geziyor/geziyor/client"
+	"golang.org/x/exp/slices"
 )
 
 /*
@@ -20,9 +23,9 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 */
 
-func fetchForm(url string) error {
+var result = make(map[string]bool)
 
-	result := make([]endpoint, 0)
+func makeRequest(url string) error {
 
 	geziyor.NewGeziyor(&geziyor.Options{
 		LogDisabled: true,
@@ -30,28 +33,15 @@ func fetchForm(url string) error {
 			g.GetRendered(url, g.Opt.ParseFunc)
 		},
 		ParseFunc: func(g *geziyor.Geziyor, r *client.Response) {
-			doc := r.HTMLDoc
-
-			if doc != nil {
-				doc.Find("form").Each(func(i int, form *goquery.Selection) {
-					ep := endpoint{}
-					ep.ref = "form"
-					ep.method = form.AttrOr("method", "GET")
-					ep.url = form.AttrOr("action", "/")
-					ep.c_type = form.AttrOr("enctype", "x-www-form-urlencoded")
-					ep.params = make(map[string]string)
-
-					form.Find("input").Each(func(i int, input *goquery.Selection) {
-
-						param := input.AttrOr("name", "")
-						ep.params[param] = input.AttrOr("value", "")
-					})
-
-					result = append(result, ep)
-					fmt.Println("result is:", result)
-				})
-
+			if slices.Contains(r.Header["Content-Type"], "application/javascript") {
+				fmt.Println("fuck, it's a js file!")
+				fetchXHR(string(r.Body))
+			} else {
+				if doc := r.HTMLDoc; doc != nil {
+					fetchForm(doc)
+				}
 			}
+
 		},
 	}).Start()
 
@@ -59,23 +49,81 @@ func fetchForm(url string) error {
 	return nil
 }
 
-func fetchJquery(url string) error {
+func fetchForm(doc *goquery.Document) {
+	result := make([]endpoint, 0)
+	doc.Find("form").Each(func(i int, form *goquery.Selection) {
+		ep := endpoint{}
+		ep.ref = "form"
+		ep.method = form.AttrOr("method", "GET")
+		ep.url = form.AttrOr("action", "/")
+		ep.c_type = form.AttrOr("enctype", "x-www-form-urlencoded")
+		ep.params = make([]string, 1)
+
+		form.Find("input").Each(func(i int, input *goquery.Selection) {
+
+			param := input.AttrOr("name", "")
+			ep.params = append(ep.params, param)
+			// ep.params[param] = input.AttrOr("value", "")
+		})
+
+		makeMap(ep)
+		result = append(result, ep)
+		fmt.Println("result is:", result)
+	})
+
+	doc.Find("script").Each(func(i int, scr *goquery.Selection) {
+		if code := scr.Text(); code != "" {
+			fetchXHR(scr.Text())
+		}
+
+	})
+
+}
+
+func fetchXHR(code string) error {
+	re, _ := regexp.Compile(`var ([a-zA-Z1-9]+)\s?=\s?new XMLHttpRequest`)
+	varName := re.FindStringSubmatch(code)
+	if varName != nil {
+		fmt.Println("var name is: ", varName[1])
+		re2, _ := regexp.Compile(varName[1] + `\.open\((.*)\)`)
+		// fmt.Println("regex is: ", re2)
+
+		if url := re2.FindStringSubmatch(code); url != nil {
+			re3, _ := regexp.Compile(varName[1] + `\.send\((.*)\)`)
+			if body := re3.FindStringSubmatch(code); body != nil {
+				fmt.Println("body is: ", body)
+			}
+			fmt.Println("url is: ", strings.Split(url[1], ",")[1])
+
+		}
+	}
 
 	return nil
 }
 
+func makeMap(ep endpoint) {
+
+	key := ep.method + "_" + ep.url + "_" + strings.Join(ep.params, ",") + "_" + ep.ref
+	fmt.Println("key is:", key)
+	if !result[key] {
+		fmt.Println("fuck!! we found a new one.")
+	}
+	//
+}
+
 type endpoint struct {
-	url    string
-	params map[string]string
+	url string
+	// params map[string]string
+	params []string
 	method string //get|post|put|delete
-	c_type string
-	ref    string //form|XHR|Axios|Jquery
+	c_type string //form|json|file
+	ref    string //form|XHR|Jquery|Axios
 }
 
 func main() {
 
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
-		fetchForm(sc.Text())
+		makeRequest(sc.Text())
 	}
 }
